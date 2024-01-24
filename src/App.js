4695fa83a306a38a3d6b0d57ca1567c7909b8cfd
uhanuu <hwryu990927@gmail.com>
2024-01-24 10:54:54 +0900

    feat: List component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import List from "./components/List";
+import List from "./components/Lists";
 import Form from "./components/Form";
 
 export default function App() {
@@ -23,14 +23,13 @@ export default function App() {
   };
 
   return (
-    <div className="container">
-      <div className="todoBlock">
-        <div className="title">
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
         </div>
-
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
         <List todoData={todoData} setTodoData={setTodoData} />
+
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
     </div>
--- src/components/List.js
+++ src/components/List.js
@@ -1,54 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function List({ todoData, setTodoData }) {
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
+const List = React.memo(
+  ({ id, title, completed, todoData, setTodoData, provided, snapshot }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
 
-  const handleCompleteChange = (id) => {
-    const newTodoData = todoData.map((d) => {
-      if (d.id === id) {
-        d.completed = !d.completed;
-      }
-      return d;
-    });
-    setTodoData(newTodoData);
-  };
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
+    const handleClick = (id) => {
+      let newTodoData = todoData.filter((data) => data.id !== id);
+      setTodoData(newTodoData);
+      localStorage.setItem("todoData", JSON.stringify(newTodoData));
     };
-  };
 
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((d) => d.id !== id); //새로운 주소값 0x1004
-    setTodoData(newTodoData);
-  };
+    const handleCompleteChange = (id) => {
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) {
+          data.completed = !data.completed;
+        }
+        return data;
+      });
+      setTodoData(newTodoData);
+      localStorage.setItem("todoData", JSON.stringify(newTodoData));
+    };
 
-  return (
-    <div>
-      {todoData.map((d) => (
-        <div style={getStyle(d.completed)} key={d.id}>
-          <input
-            type="checkbox"
-            defaultChecked={false}
-            onChange={() => handleCompleteChange(d.id)}
-          />
-          {d.title}
-          <button style={btnStyle} onClick={() => handleClick(d.id)}>
-            x
-          </button>
+    const handleEditChange = (e) => {
+      setEditedTitle(e.target.value);
+    };
+
+    const handleSubmit = () => {
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) {
+          data.title = editedTitle;
+        }
+        return data;
+      });
+      setTodoData(newTodoData);
+      localStorage.setItem("todoData", JSON.stringify(newTodoData));
+      setIsEditing(false);
+    };
+
+    if (isEditing) {
+      return (
+        <div className="flex items-center justify-between w-full px-4 py-1 my-1 text-gray-600 bg-gray-100 border rounded row">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="w-full px-3 py-2 mr-4 text-gray-500 appearance-none"
+              value={editedTitle}
+              onChange={handleEditChange}
+              autoFocus
+            />
+          </form>
+          <div className="items-center">
+            <button
+              class="px-4 py-2 float-right"
+              onClick={() => setIsEditing(false)}
+              type="button"
+            >
+              x
+            </button>
+            <button
+              onClick={handleSubmit}
+              class="px-4 py-2 float-right"
+              type="submit"
+            >
+              save
+            </button>
+          </div>
         </div>
-      ))}
-    </div>
-  );
-}
+      );
+    } else {
+      return (
+        <div
+          key={id}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+          className={`${
+            snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+          } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+        >
+          <div className="items-center">
+            <input
+              type="checkbox"
+              onChange={() => handleCompleteChange(id)}
+              defaultChecked={completed}
+            />{" "}
+            <span className={completed ? "line-through" : undefined}>
+              {title}
+            </span>
+          </div>
+          <div className="items-center">
+            <button
+              className="float-right px-4 py-2"
+              onClick={() => handleClick(id)}
+            >
+              x
+            </button>
+            <button
+              className="float-right px-4 py-2"
+              onClick={() => setIsEditing(true)}
+            >
+              edit
+            </button>
+          </div>
+        </div>
+      );
+    }
+  }
+);
+
+export default List;
